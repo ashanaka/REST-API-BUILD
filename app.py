4085810b2183695dc187ec0e353a31f6bebe5ba9
ashanaka <jpashanshanaka@gmail.com>
2020-11-20 20:31:20 +0530

    first post request

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
 
+@app.route('/bye')
+def bye():
+    retJson = {
+                'field1': "field 1 data",
+                'field2': "field 2 data"
+            }
+    return jsonify(retJson)
+
+@app.route('/add', methods=["POST"])
+def add():
+    reqDict = request.get_json()
+    x = reqDict["x"]
+    y = reqDict["y"]
+    z = x+y
+    retJson = {
+                'z': z
+            }
+    return jsonify(retJson), 200
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=true)
